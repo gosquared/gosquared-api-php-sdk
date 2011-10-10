47ba9bfe7b4ae22275ab2ffe2e2778cf23bbd52d
Geoff Wagstaff <geoff@gosquared.com>
2011-10-11 00:48:44 +0100

    Added tests

--- test/test.php
+++ test/test.php
@@ -1,12 +1,12 @@
 <?php
 require_once(dirname(__FILE__) . '/simpletest/autorun.php');
 require_once(dirname(__FILE__) . '/simpletest/unit_tester.php');
-require '../sdk.class.php';
+require __DIR__.'/../sdk.class.php';
 
 class TestResponses extends UnitTestCase{
     function testFunctions(){
         // Lazy but quick
-        $functions = array_map(function($e){return substr(basename($e), 0, -4);}, array_diff(glob('../lib/*.php'), array('../lib/gosquared_func.php')));
+        $functions = array_map(function($e){return substr(basename($e), 0, -4);}, array_diff(glob(__DIR__.'/../lib/*.php'), array(__DIR__.'/../lib/gosquared_func.php')));
         foreach($functions as $function){
             $g = new gosquared\sdk('GSN-2194840-F');
             $r = $g->$function();
